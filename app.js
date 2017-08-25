b2fe9f66504c480b2db11e8c165c147d14348cb0
lebangu <lebangu11t1@gmail.com>
2017-08-25 14:47:03 +0700

    fix

--- app.js
+++ app.js
@@ -28,36 +28,6 @@ app.set('views', 'app/views');
 var routes = require('./routes/web');
 routes(app);
 
-app.locals.show_parent = function(parent_id) {
-
-    var data = '';
-
-    mysqlPromise.createConnection({
-        host: '192.168.0.253',
-        user: 'VNCHATAPP',
-        password: 'cJoHAa9bJoHAPj6X98jc',
-        database: 'VNCHATAPP'
-    }).then(function(conn){
-        var result = conn.query(`SELECT users.username, users.avatar, circle_post_comments.body 
-        FROM users INNER JOIN circle_post_comments ON users.id = circle_post_comments.user_id 
-        WHERE circle_post_comments.id=${parent_id} LIMIT 1`);
-        conn.end();
-        return result;
-    }).then(function(rows){
-        data = rows[0].username;
-    });
-
-    console.log(data);
-
-    // let data = [];
-    // var query = `SELECT users.username, users.avatar, circle_post_comments.body FROM users INNER JOIN circle_post_comments ON users.id = circle_post_comments.user_id WHERE circle_post_comments.id=${parent_id} LIMIT 1`;
-    // con.query(query, function (error, results, fields) {
-    //     if (error) throw error;
-    //     data = results[0];
-    // });
-    // console.log(data);
-}
-
 app.listen(port, function () {
     console.log(`Server is listening ${port}`)
 });
