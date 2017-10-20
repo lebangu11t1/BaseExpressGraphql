4848a6adbb9842bce2d1b3967b6cde821eb6be0d
lebangu11t1 <lebangu11t1@gmail.com>
2017-10-20 16:01:26 +0700

    connect db

--- app/config/database.js
+++ app/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     "mysql" : {
-        "host": "192.168.0.253",
-        "user": "VNCHATAPP",
-        "password": "cJoHAa9bJoHAPj6X98jc",
-        "database": 'VNCHATAPP'
+        "host": "127.0.0.1",
+        "user": "VN_CHATAPP",
+        "password": "L5MF9f2vrb56rJmkhMLEz2",
+        "database": 'VN_CHATAPP'
     },
     "mongo" : {
         // do something
